87d312ef9c2df13ed6a5e07b6af9cb8ae186d5a8
Matthijs de Wit <matthijs111@gmail.com>
2020-11-05 00:29:41 +0100

    clearer plots

--- assignment1/i_vs_s.py
+++ assignment1/i_vs_s.py
@@ -17,13 +17,19 @@ if __name__ == "__main__":
     imag_bounds = [-1.25, 1.25]
 
     s_std_results = []
+    s_mean_results = []
     for s in s_list:
         individual_results = []
         for _ in range(10):
             a = random_sampling_method(int(s), real_bounds, imag_bounds, 10000, False)
             individual_results.append(a)
         s_std_results.append(np.std(individual_results))
+        s_mean_results.append(np.mean(individual_results) - real_A_M)
     
+    s_std_results = np.array(s_std_results)
+    s_mean_results = np.array(s_mean_results)
+
+    i_std_results = []
     i_mean_results = []
     for i in i_list:
         print(i)
@@ -31,15 +37,21 @@ if __name__ == "__main__":
         for _ in range(10):
             a = random_sampling_method(10000, real_bounds, imag_bounds, int(i), False)
             individual_results.append(a)
+        i_std_results.append(np.std(individual_results))
         i_mean_results.append(np.mean(individual_results) - real_A_M)
 
-    axs[0].plot(s_list, s_std_results)
+    i_std_results = np.array(i_std_results)
+    i_mean_results = np.array(i_mean_results)
+
+    axs[0].plot(s_list, s_mean_results)
+    axs[0].fill_between(s_list, s_mean_results - s_std_results, s_mean_results + s_std_results, alpha=0.5)
     axs[0].set_xlabel('num_samples')
-    axs[0].set_ylabel('standard deviation')
+    axs[0].set_ylabel('deviation from real A_M')
     axs[0].set_xscale('log')
     axs[0].plot([0, max(s_list)], [0, 0], '--')  # show 0 line
 
     axs[1].plot(i_list, i_mean_results)
+    axs[1].fill_between(i_list, i_mean_results - i_std_results, i_mean_results + i_std_results, alpha=0.5)
     axs[1].set_xlabel('max_iterations')
     axs[1].set_ylabel('deviation from real A_M')
     axs[1].set_xscale('log')
