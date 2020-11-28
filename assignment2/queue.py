976bc5edc461bdeb31b8298613a14818c678e5be
mbruin <menno.bruin@student.uva.nl>
2020-11-28 13:15:23 +0100

    Added DES_n124

--- assignment2/queue.py
+++ assignment2/queue.py
@@ -1,47 +1,70 @@
 import simpy
 import random
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
 
 class Server:
-    def __init__(self, env, n):
+    def __init__(self, env, n, shortest_job_first=False):
         self.env = env
-        self.resources = simpy.Resource(env, capacity=n)
-        # self.resources = simpy.PriorityResource(env, capacity=n)
+        self.resources = simpy.Resource(env, capacity=n) if not shortest_job_first else simpy.PriorityResource(env, capacity=n)
 
-    def execute_job(self, id, service_time):
+    def execute_job(self, service_time):
         yield self.env.timeout(service_time)
 
 
-def job_generator(env, server):
+def job_generator(env, server, arrival_rate, shortest_job_first=False):
     for i in range(NUM_REQUESTS):
-        j = job(env, i, server)
+        j = job(env, i, server, shortest_job_first)
         env.process(j)
 
-        # wait for next arrival event
-        yield env.timeout(ARRIVAL_RATE)
+        yield env.timeout(arrival_rate)
 
 
-def job(env, id, server):
-    print(f"{id} arrives at {env.now}")
+def job(env, id, server, shortest_job_first):
+    # print(f"{id} arrives at {env.now:.1f}s")
 
-    # request allocation of resource for job
-    # with server.resources.request(priority=10-id) as request:
-    with server.resources.request() as request:
-        yield request
-        print(f"{id} starts running at {env.now}")
+    t = random.uniform(MIN_SERVICE_TIME, MAX_SERVICE_TIME)
 
-        # run job
-        yield env.process(server.execute_job(id, 10-id))
-        print(f"{id} finished at {env.now}")
+    if shortest_job_first:
+        request = server.resources.request(priority=t)
+    else:
+        request = server.resources.request()
+
+    with request as r:
+        yield r
+        # print(f"{id} starts running at {env.now:.1f}s")
+
+        yield env.process(server.execute_job(service_time=t))
+        # print(f"{id} finished at {env.now:.1f}s")
 
 
-NUM_REQUESTS = 10
+NUM_REQUESTS = 1000
 ARRIVAL_RATE = 5  # new request every [x] seconds
-SERVICE_TIME = 6  # [x] second to complete job
+MIN_SERVICE_TIME = 2
+MAX_SERVICE_TIME = 20
+SJF = False  # shortest job first
 
-for n in [1]:
-    print(f"------------ n = {n} -------------")
-    env = simpy.Environment()
-    server = Server(env, n)
-    env.process(job_generator(env, server))
-    env.run()
+average_waiting_times = []
+
+for n in [1, 2, 4]:
+    arrival_rate = ARRIVAL_RATE / n
+    print(f"\r------------ n = {n} -------------", end='')
+    for i in range(500):
+        env = simpy.Environment()
+        server = Server(env, n, shortest_job_first=SJF)
+        env.process(job_generator(env, server, arrival_rate, shortest_job_first=SJF))
+        env.run()
+
+        average_waiting_time = env.now / NUM_REQUESTS - arrival_rate
+        average_waiting_times.append(average_waiting_time)
+
+    sns.distplot(np.array(average_waiting_times), label=f'n={n}')
+    average_waiting_times = []
+
+plt.xlabel('Average Waiting Time')
+plt.ylabel('# Measurements')
+plt.legend()
+plt.show()
